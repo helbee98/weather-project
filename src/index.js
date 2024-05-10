ea74adf3b796a5ba1939350e7b5ab9f68ac45e44
helbee98 <helenabeech@hotmail.com>
2024-05-10 10:27:54 +0100

    remove test add api

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = math.round(temperature);
+}
+
+function searchCity(city) {
+    let apiKey = "f0t31ebf10a05340167073caaaa44dbo";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value)
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-alert("Hello! I am an alert box!!");
 
+searchCity("paris");
